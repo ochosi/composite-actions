47bd0749382867466093c0e80184c61a6ff5a76f
Simon Steinbeiss <simon.steinbeiss@redhat.com>
2021-12-10 21:56:50 +0100

    Try to fix bash substitution

--- release-info.sh
+++ release-info.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TAG=$(git describe "${GITHUB_REF}")
+TOPLEVEL=$(git rev-parse --show-toplevel)
 git tag --list --format="%(subject)%0a%(body)" $TAG > release.md
 echo "release_version=${TAG//v}" >> $GITHUB_ENV
-echo "component=${basename `git rev-parse --show-toplevel`}" >> $GITHUB_ENV
+echo "component=${basename $TOPLEVEL}" >> $GITHUB_ENV
